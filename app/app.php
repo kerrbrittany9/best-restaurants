97f480c43c0701a36f2b757a6440f63f37939319
Brittany Kerr and Dylan Lewis <dylan@>
2017-07-12 16:37:53 -0700

    write homepage in twig

--- app/app.php
+++ app/app.php
@@ -13,6 +13,10 @@
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
 
+    $app->get("/", function() use ($app) {
+      return $app['twig']->('index.html.twig', array('cuisines' => Cuisine::getAll()));
+    });
+
 
 
 ?>
