b2cb947d26e7dd2e903b1453ea95874a35de5cfb
Brittany Kerr and Dylan Lewis <dylan@>
2017-07-13 10:02:13 -0700

    add cuisine and restaurant routes and twig templates

--- app/app.php
+++ app/app.php
@@ -1,8 +1,8 @@
 <?php
     date_default_timezone_set('America/Los_Angeles');
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src.Restaurant.php";
-    require_once __DIR__."/../src.Cuisine.php";
+    require_once __DIR__."/../src/Restaurant.php";
+    require_once __DIR__."/../src/Cuisine.php";
 
     $app = new Silex\Application();
 
@@ -14,9 +14,33 @@
     $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
 
     $app->get("/", function() use ($app) {
-      return $app['twig']->('index.html.twig', array('cuisines' => Cuisine::getAll()));
+      return $app['twig']->render('index.html.twig', array('cuisines' => Cuisine::getAll()));
     });
 
+    $app->get("/restaurants", function() use ($app) {
+      return $app['twig']->render('restaurants.html.twig', array('restaurants' => Restaurant::getAll()));
+    });
 
+    $app->get("/cuisine/{id}", function($id) use ($app) {
+      $cuisine = Cuisine::find($id);
+      return $app['twig']->render('cuisine.html.twig', array('cuisine' => $cuisine, 'restaurants' => $cuisine->getRestaurants()));
+    });
+
+    $app->post("/cuisine", function() use ($app) {
+      $cuisine = new Cuisine($_POST['type']);
+      $cuisine->save();
+      return $app['twig']->render('index.html.twig', array('cuisines' => Cuisine::getAll()));
+    });
+
+    $app->post("/restaurants", function() use ($app) {
+      $name = $_POST['input_name'];
+      $cuisine_id = $_POST['cuisine_id'];
+      $restaurant = new Restaurant($name, $cuisine_id);
+      $restaurant->save();
+      $cuisine = Cuisine::find($cuisine_id);
+      return $app['twig']->render('cuisine.html.twig', array('cuisine' => $cuisine, 'restaurants' => $cuisine->getRestaurants()));
+    });
+
+    return $app;
 
 ?>
