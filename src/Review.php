c7fafc89c96533c27aa72ef8852efb920d4c957e
Brittany Kerr and Dylan Lewis <brittany@>
2017-07-13 16:54:17 -0700

    add review

--- src/Review.php
+++ src/Review.php
@@ -49,6 +49,10 @@
               }
         }
 
+        static function getAll()
+        {
+
+        }
 
     }
 
--- tests/ReviewTest.php
+++ tests/ReviewTest.php
@@ -27,6 +27,21 @@
           $this->assertTrue($executed, "Review was not saved to database");
 
       }
+      function testGetAll()
+      {
+          $title = "Lovely Venue";
+          $summary = "i took my grandma and she had a ball";
+          $test_review = new Review($title, $summary);
+          $test_review->save();
+          $title2 = "i loved the food";
+          $summary2 = "it was the best meal of my life";
+          $test_review2 = new Review($title2, $summary2);
+          $test_review2->save();
+
+          $result = Review::getAll();
+
+          $this->assertEquals([$test_review, $test_review2], $result);
+      }
 
     }
 
